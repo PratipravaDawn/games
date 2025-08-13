016628f837917e47da443252e7b8639f4a6198c9
Pratiprava Dawn <118844413+PratipravaDawn@users.noreply.github.com>
2025-08-13 21:01:12 +0530

    Add files via upload

---
+++ CurrencyConverter/script.js
@@ -0,0 +1,224 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+const BASE_URL =
+  "https://latest.currency-api.pages.dev/v1/currencies/eur.json"
+
+
+let dropdowns = document.querySelectorAll('.dropdown select');
+let convertBtn = document.querySelector('button');
+let fromCurr = document.querySelector('select#from');
+let toCurr = document.querySelector('select#to');
+let msg = document.querySelector('.message');
+
+for (let select of dropdowns) {
+    for (Currcodes in countryList){
+        let newOption = document.createElement('option');
+        newOption.innerText = Currcodes;
+        newOption.value = Currcodes;
+        if (newOption.value === 'USD' && select.id === 'from') {
+            newOption.selected = "selected";}
+        if (newOption.value === 'INR' && select.id === 'to') {
+        newOption.selected = "selected";}
+        select.append(newOption);
+    }
+    select.addEventListener('change', (evt) =>{
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    Currcodes = element.value;
+    countryCode = countryList[Currcodes];
+    let image = document.querySelector(`.${element.id} img`);
+    image.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+}
+
+const updateCurency = async () => {
+    let amt = document.querySelector('input#amount');
+    let amount = amt.value;
+    if (amount === '' || amount <= 0) {
+        amt.value = 1;
+        amount = 1;
+    }
+
+    let res = await fetch(BASE_URL);
+    let data = await res.json();
+    let euroMid = data['eur'];
+
+    let fromCurrvalue = euroMid[(fromCurr.value).toLowerCase()];
+    let toCurrvalue = euroMid[(toCurr.value).toLowerCase()];
+
+
+    let rate = (1 / fromCurrvalue) * toCurrvalue;
+    let finalAmount = (amount * rate).toFixed(2);
+
+    msg.innerText =`${amount} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+convertBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    updateCurency();
+});
+
+window.addEventListener('load', () => {
+    updateCurency();
+});
